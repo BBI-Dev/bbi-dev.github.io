828b00d82998145a45a4168b54a62d549cda1569
BBI-Dev <163919404+BBI-Dev@users.noreply.github.com>
2024-05-10 09:11:44 +1200

    Create admin2js.js

---
+++ insider/source/admin2js.js
@@ -0,0 +1,7 @@
+
+      function viewgitpass(){
+        document.getElementById("gitpass").style.display = "block";
+        setTimeout(function() {
+  document.getElementById("gitpass").style.display = "none";
+}, 500);
+      }
