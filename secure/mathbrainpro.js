fff98a2a707ea2782a5c72906db0bee31d6c1ee5
BBI-Dev <163919404+BBI-Dev@users.noreply.github.com>
2024-05-20 12:10:09 +1200

    Update mathbrainpro.js

--- secure/mathbrainpro.js
+++ secure/mathbrainpro.js
@@ -1,2 +1,14 @@
+function check(){
+  let value = document.getElementById("pass").value;
+  for (let i = 0; i < 5; i++) {
+  array=eval('user'+count);
+  let username = array.at(count-1);
+  if(value==username){
+    alert('success')
+  }
+  let count=count+1;
+}
+
 const length = 1;
-const user1= ["admin2", 2024, 6, 23];
+const user1 = ["admin2", 2024, 6, 23];
+let count=1;
