3454d3c0f1a8fa07fe3fb06ecb68252427695b47
BBI-Dev <163919404+BBI-Dev@users.noreply.github.com>
2025-08-08 16:52:04 +1200

    Delete insider/source/admin2js.js

--- insider/source/admin2js.js
+++
@@ -1,7 +0,0 @@
-
-      function viewgitpass(){
-        document.getElementById("gitpass").style.display = "block";
-        setTimeout(function() {
-  document.getElementById("gitpass").style.display = "none";
-}, 500);
-      }
